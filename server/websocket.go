27c316ed8d70e5a1c7f4685babcb388f073842d6
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-04-23 19:22:24 +0800

    fix: client.uin

--- server/websocket.go
+++ server/websocket.go
@@ -239,7 +239,7 @@ func (c *websocketClient) connect(typ, addr string, conptr **wsConn) {
 	log.Infof("开始尝试连接到反向WebSocket %s服务器: %v", typ, addr)
 	header := http.Header{
 		"X-Client-Role": []string{typ},
-		"X-Self-ID":     []string{strconv.FormatInt(c.bot.Client.Uin, 10)},
+		"X-Self-ID":     []string{strconv.FormatInt(int64(c.bot.Client.Uin), 10)},
 		"User-Agent":    []string{"CQHttp/4.15.0"},
 	}
 	if c.token != "" {
